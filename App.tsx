ad892e0b7bfca17f8e20c1dbe78eed4447f9c925
seymanurks <nurks1905@gmail.com>
2024-02-17 18:42:14 +0300

    switch and flatlist

--- App.tsx
+++ App.tsx
@@ -1,118 +1,39 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React, {useState} from "react";
+import {SafeAreaView, View, Text, FlatList, Switch} from "react-native"
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+const data = [
+  {id: 0, name: 'Kafe Kodlama', isFavorite: true},
+  {id: 1, name: 'Bug Kafe', isFavorite: false},
+  {id: 2, name: 'Tekno Kafe', isFavorite: false},
+  {id: 3, name: 'KafeKod', isFavorite: true},
+  {id: 4, name: 'Kodlayan Kafe', isFavorite: false},
+  {id: 5, name: 'Veri Durağı', isFavorite: true},
+  ];
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const App = () => {
+  const [cafeList, setCafeList] = useState(data)
+  const [showOnlyFavorites, setShowOnlyFavorites] = useState(false)
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
+  function onFavoritesChange(isFavoriteSelected) {
+    setShowOnlyFavorites(isFavoriteSelected)
+    isFavoriteSelected 
+    ? setCafeList(cafeList.filter(cafe => cafe.isFavorite))
+    : setCafeList(data)
+  }
+ 
+  return(
+    <SafeAreaView>
+      <View style = {{margin: 10}}>
+        <Text style = {{fontSize: 20}}>Show Only Favorites</Text>
+        <Switch value = {showOnlyFavorites} onValueChange = {onFavoritesChange}/>
+      </View>
+  
+      <FlatList
+      data = {cafeList}
+      renderItem = {({item}) => <Text style = {{fontSize: 18}}>{item.name}</Text>}
       />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
     </SafeAreaView>
-  );
+  )
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
